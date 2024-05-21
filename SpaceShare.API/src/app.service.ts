1141aabacb19a0ef574f3cddfb7eb81394889126
Jhean Galope <drakekhendrick@gmail.com>
2024-05-22 02:02:16 +0800

    test api pipeline

--- SpaceShare.API/src/app.service.ts
+++ SpaceShare.API/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): Object {
-    return {message: "testing world", state: "tired"};
+    return {message: "testing world", state: "update"};
   }
 }
