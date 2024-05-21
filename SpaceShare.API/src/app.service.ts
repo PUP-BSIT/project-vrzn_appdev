b8f5f5dbe240102a925bf90a76a0f5f2fe0f32e1
Jhean Galope <drakekhendrick@gmail.com>
2024-05-21 19:12:01 +0800

    test cicd

--- SpaceShare.API/src/app.service.ts
+++ SpaceShare.API/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): Object {
-    return {message: "testing world"};
+    return {message: "testing world", state: "tired"};
   }
 }
