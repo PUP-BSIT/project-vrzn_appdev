a17cf2602d20b6515c8faf2508fcdfea0d4e317a
Jhean Galope <drakekhendrick@gmail.com>
2024-05-21 16:04:50 +0800

    add test api call

--- SpaceShare.UI/src/app/app.component.ts
+++ SpaceShare.UI/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'], // Change "styleUrl" to "styleUrls"
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  // Implement OnInit interface
   title = 'SpaceShare.UI';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Implement ngOnInit method
+    this.http.get<string>('http://localhost:3000/').subscribe((data) => {
+      console.log(data);
+      return data;
+    });
+  }
 }
