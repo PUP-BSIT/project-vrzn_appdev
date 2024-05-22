d15fd69443db85391f5276864333aaec57023190
Jhean Galope <drakekhendrick@gmail.com>
2024-05-22 18:48:21 +0800

    add var swap depending on environment

--- SpaceShare.API/src/app.service.ts
+++ SpaceShare.API/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): Object {
-    return {message: "testing world", state: "update"};
+    return {message: "testing world", state: "development"};
   }
 }
