7fa9c1cfe83eec9e23e044282d4410b0ff3decd8
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-05-28 12:20:03 +0800

    remove unnecessary code

--- SpaceShare.UI/src/app/landing/login/login.component.ts
+++ SpaceShare.UI/src/app/landing/login/login.component.ts
@@ -3,14 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  // Initially set to false to hide the modal
-  isModalOpen: boolean = false;
 
-  // Method to close the modal
-  closeModal(): void {
-    this.isModalOpen = false;
-  }
 }
