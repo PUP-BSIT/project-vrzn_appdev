c0aa3b89f766feccf119b0f30b8a5b7d7cd552dc
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-05-28 11:28:11 +0800

    modify login component

--- SpaceShare.UI/src/app/landing/login/login.component.ts
+++ SpaceShare.UI/src/app/landing/login/login.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  // Initially set to false to hide the modal
+  isModalOpen: boolean = false;
 
+  // Method to close the modal
+  closeModal(): void {
+    this.isModalOpen = false;
+  }
 }
