13571039f8aab3a15e8d9c1ea12b86b03ee388e0
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-05-28 15:59:59 +0800

    added structure and design of loggedin header in its component

---
+++ SpaceShare.UI/src/app/header-loggedin/header-loggedin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-loggedin',
+  templateUrl: './header-loggedin.component.html',
+  styleUrl: './header-loggedin.component.css'
+})
+export class HeaderLoggedinComponent {
+
+}
