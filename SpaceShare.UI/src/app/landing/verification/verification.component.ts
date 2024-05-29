ebdd0ee1f8bfc1a41357e28e638d96969dacc682
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-05-29 11:53:05 +0800

    remove function in otp pg

--- SpaceShare.UI/src/app/landing/verification/verification.component.ts
+++ SpaceShare.UI/src/app/landing/verification/verification.component.ts
@@ -33,17 +33,11 @@ export class VerificationComponent implements OnInit {
     this.closeModal();
   }
 
-  openModal() {
-    const modal = document.getElementById('my_modal_3') as HTMLDialogElement;
-    if (modal) {
-      modal.showModal();
-    }
-  }
-
   closeModal() {
     const modal = document.getElementById('my_modal_3') as HTMLDialogElement;
     if (modal) {
       modal.close();
     }
   }
+
 }
