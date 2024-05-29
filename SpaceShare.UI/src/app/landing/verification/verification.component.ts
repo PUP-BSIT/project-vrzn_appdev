525cb6da54ddc58585b626da80b3028ad631cc24
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-05-29 11:25:34 +0800

    add validation on otp

--- SpaceShare.UI/src/app/landing/verification/verification.component.ts
+++ SpaceShare.UI/src/app/landing/verification/verification.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-verification',
   templateUrl: './verification.component.html',
-  styleUrl: './verification.component.css'
+  styleUrls: ['./verification.component.css']
 })
-export class VerificationComponent {
+export class VerificationComponent implements OnInit {
+  otpForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.otpForm = this.formBuilder.group({
+      otp: [null, [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
+    });
+  }
+
+  get otpControl(): AbstractControl {
+    return this.otpForm.get('otp')!;
+  }
+
+  clearError(controlName: string) {
+    this.otpForm.get(controlName)?.markAsUntouched();
+  }
+
+  onSubmit() {
+    if (!this.otpForm.valid) {
+      return;
+    }
+    console.log(this.otpForm.value);
+    this.closeModal();
+  }
+
+  openModal() {
+    const modal = document.getElementById('my_modal_3') as HTMLDialogElement;
+    if (modal) {
+      modal.showModal();
+    }
+  }
+
+  closeModal() {
+    const modal = document.getElementById('my_modal_3') as HTMLDialogElement;
+    if (modal) {
+      modal.close();
+    }
+  }
 }
