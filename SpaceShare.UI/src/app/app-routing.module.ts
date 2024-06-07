e9c05f83d54b2e433e6b0792e0070d004b5f277f
Jhean Galope <drakekhendrick@gmail.com>
2024-06-07 16:10:41 +0800

    remove unused imports

--- SpaceShare.UI/src/app/app-routing.module.ts
+++ SpaceShare.UI/src/app/app-routing.module.ts
@@ -5,21 +5,13 @@ import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './landing/register/register.component';
 import { LoginComponent } from './landing/login/login.component';
 import { AgreementComponent } from './landing/agreement/agreement.component';
-import { PropertyCardComponent } from './property_card/property-card.component';
 import { PropertyComponent } from './property/property.component';
 import { VerificationComponent } from './landing/verification/verification.component';
 import { ListingComponent } from './listing/listing.component';
 import { AddListingComponent } from './add-listing/add-listing.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SuccessComponent } from './success/success.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './landing/navbar/navbar.component';
 import { WishlistComponent } from './wishlist/wishlist.component';
-import { CarouselComponent } from './property/carousel/carousel.component';
-import { FeaturesComponent } from './property/features/features.component';
-import { InfoComponent } from './property/info/info.component';
-import { DealComponent } from './property/info/deal/deal.component';
-import { HostComponent } from './property/features/host/host.component';
 import { TermsComponent } from './landing/agreement/terms/terms.component';
 import { PrivacyComponent } from './landing/agreement/privacy/privacy.component';
 import  { ResetPasswordComponent } from './landing/reset_password/reset-password.component';
@@ -34,10 +26,10 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'agreement', component: AgreementComponent },
-  { path: 'property/:id', component: PropertyComponent }, // Dynamic route for property details
+  { path: 'property/:id', component: PropertyComponent },
   { path: 'verification', component: VerificationComponent },
   { path: 'listing', component: ListingComponent },
-  { path: 'listing/add', component: AddListingComponent }, // Nested route for adding listings
+  { path: 'listing/add', component: AddListingComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'success', component: SuccessComponent },
   { path: 'wishlist', component: WishlistComponent },
