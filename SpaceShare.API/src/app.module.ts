b099f96aece93df8617b31fb38100814f22d034b
Jhean Galope <drakekhendrick@gmail.com>
2024-06-09 04:26:48 +0800

    update mailer module declaration

--- SpaceShare.API/src/app.module.ts
+++ SpaceShare.API/src/app.module.ts
@@ -5,8 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
 import { PropertyModule } from './property/property.module';
 import { S3Module } from './s3/s3.module';
-import { MailerModule, MailerService } from '@nestjs-modules/mailer';
-import { mailerConfig } from './auth/mailer/mailer.config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -14,7 +13,21 @@ import { mailerConfig } from './auth/mailer/mailer.config';
     PrismaModule,
     PropertyModule,
     S3Module,
-    MailerModule.forRoot(mailerConfig),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        defaults: {
+          from: '"SpaceShare" <spacesharevrzn@gmail.com>',
+        },
+        preview: true,
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
