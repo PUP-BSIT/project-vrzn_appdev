d5c6b76f5ff75b5961a9ee9158f8be874c4c82b1
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-06-10 13:29:03 +0800

    add dob custom validator

---
+++ SpaceShare.UI/src/app/landing/register/custom-validators.ts
@@ -0,0 +1,26 @@
+// custom-validators.ts
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export class CustomValidators {
+  static adultAgeValidator(fieldName: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const birthdate = control.value;
+
+      if (!birthdate) {
+        return null; 
+      }
+
+      // Calculate age based on the birthdate
+      const today = new Date();
+      const birthDate = new Date(birthdate);
+      const age = today.getFullYear() - birthDate.getFullYear();
+
+      // Check if age is less than 18
+      if (age < 18) {
+        return { 'adultAge': { fieldName } }; 
+      }
+
+      return null; 
+    };
+  }
+}
