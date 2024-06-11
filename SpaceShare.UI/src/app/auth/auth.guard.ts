648eedd51664098a8cd134cfc9c0f521c804c266
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-06-11 22:22:23 +0800

    Add AuthGuard file

---
+++ SpaceShare.UI/src/app/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    let isLoggedIn = this.authService.isAuthenticated();
+    if (isLoggedIn) {
+      return true;
+    } else {
+      this.router.navigate(['/main']);
+      return false;
+    }
+  }
+}
