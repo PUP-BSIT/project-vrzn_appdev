964a128040f44b62c0781d2dac109a60de559457
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-06-14 10:06:59 +0800

    create cookie component

---
+++ SpaceShare.UI/src/app/landing/agreement/cookie/cookie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cookie',
+  templateUrl: './cookie.component.html',
+  styleUrl: './cookie.component.css'
+})
+export class CookieComponent {
+
+}
