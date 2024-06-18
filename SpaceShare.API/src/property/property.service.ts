a710647c7b13a636eafec26ed31b5501fad824aa
Jhean Galope <drakekhendrick@gmail.com>
2024-06-18 08:50:24 +0800

    add images in return property api

--- SpaceShare.API/src/property/property.service.ts
+++ SpaceShare.API/src/property/property.service.ts
@@ -26,6 +26,11 @@ export class PropertyService {
         status: true,
         rating: true,
         capacity: true,
+        images: {
+          select: {
+            image_url: true,
+          }
+        }
       },
     });
   }
