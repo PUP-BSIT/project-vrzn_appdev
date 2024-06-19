b538aa9c21515e5ec6c5a3167d0efe70f9ec4898
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-06-19 18:01:29 +0800

    apply agreement style to cookie component

--- SpaceShare.UI/src/app/landing/agreement/about/about.component.ts
+++ SpaceShare.UI/src/app/landing/agreement/about/about.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['../agreement.component.css']
+  styleUrl: '../agreement.component.css'
 })
 export class AboutComponent {}
