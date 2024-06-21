e4d00098fe5a3026771f422044c05520a266c4ae
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-06-22 04:47:52 +0800

    refactor main component for logic handling filtration

--- SpaceShare.UI/src/app/main/main.component.ts
+++ SpaceShare.UI/src/app/main/main.component.ts
@@ -10,13 +10,25 @@ import { Subscription } from 'rxjs';
 })
 export class MainComponent implements OnInit {
   cards: Card[] = [];
+  filteredCards: Card[] = [];
   paginatedCards: Card[] = [];
   subscription!: Subscription;
   loaded = false;
 
   currentPage: number = 1;
-  itemsPerPage: number = 1; // Change "itemsPerPage" to desired value
+  itemsPerPage: number = 10;
   totalPages: number = 1;
+
+  uniqueCities: string[] = [];
+  bedroomOptions: number[] = [1, 2, 3, 4, 5];
+
+  selectedCity: string = '';
+  minPrice: number = 0;
+  maxPrice: number = 10000000;
+  selectedBedrooms: number | null = null;
+
+  isPriceDropdownOpen: boolean = false;
+  priceRangeLabel: string = 'Select Price Range';
 
   constructor(private mainService: MainService) {}
 
@@ -24,7 +36,11 @@ export class MainComponent implements OnInit {
     this.mainService.getProperties().subscribe(
       (data: Card[]) => {
         this.cards = data;
-        this.totalPages = Math.ceil(this.cards.length / this.itemsPerPage);
+        this.filteredCards = this.cards;
+        this.uniqueCities = [...new Set(this.cards.map((card) => card.city))];
+        this.totalPages = Math.ceil(
+          this.filteredCards.length / this.itemsPerPage
+        );
         this.updatePaginatedCards();
       },
       (error) => {
@@ -37,10 +53,32 @@ export class MainComponent implements OnInit {
     }, 1000);
   }
 
+  applyFilters(): void {
+    this.filteredCards = this.cards.filter((card) => {
+      return (
+        (!this.selectedCity || card.city === this.selectedCity) &&
+        (!this.selectedBedrooms || card.bedroom === this.selectedBedrooms) &&
+        card.price >= this.minPrice &&
+        card.price <= this.maxPrice
+      );
+    });
+    this.totalPages = Math.ceil(this.filteredCards.length / this.itemsPerPage);
+    this.currentPage = 1;
+    this.updatePaginatedCards();
+    this.isPriceDropdownOpen = false;
+    this.updatePriceRangeLabel();
+  }
+
+  clearPriceFilters(): void {
+    this.minPrice = 0;
+    this.maxPrice = 10000000;
+    this.applyFilters();
+  }
+
   updatePaginatedCards(): void {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
-    this.paginatedCards = this.cards.slice(startIndex, endIndex);
+    this.paginatedCards = this.filteredCards.slice(startIndex, endIndex);
   }
 
   nextPage(): void {
@@ -54,6 +92,29 @@ export class MainComponent implements OnInit {
     if (this.currentPage > 1) {
       this.currentPage--;
       this.updatePaginatedCards();
+    }
+  }
+
+  togglePriceDropdown(): void {
+    this.isPriceDropdownOpen = !this.isPriceDropdownOpen;
+  }
+
+  setPriceRange(min: number, max: number): void {
+    this.minPrice = min;
+    this.maxPrice = max;
+    this.applyFilters();
+  }
+
+  updatePriceRangeLabel(): void {
+    if (this.minPrice === 0 && this.maxPrice === 10000000) {
+      this.priceRangeLabel = 'Select Price Range';
+    } else if (this.minPrice === 0) {
+      this.priceRangeLabel = `Below PHP ${this.maxPrice.toLocaleString()}`;
+    } else if (this.maxPrice === 10000000) {
+      this.priceRangeLabel = `Above PHP ${this.minPrice.toLocaleString()}`;
+    } else {
+      this.priceRangeLabel = `PHP ${this.minPrice.toLocaleString()} 
+        - PHP ${this.maxPrice.toLocaleString()}`;
     }
   }
 }
