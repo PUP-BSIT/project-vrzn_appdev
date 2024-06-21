fc616abe92cc3b9da5f6c36f8ed7c7822491b351
Jhean Galope <drakekhendrick@gmail.com>
2024-06-21 17:47:23 +0800

    add skeleton card component

---
+++ SpaceShare.UI/src/app/card-skeleton/card-skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-skeleton',
+  templateUrl: './card-skeleton.component.html',
+  styleUrl: './card-skeleton.component.css'
+})
+export class CardSkeletonComponent {
+
+}
