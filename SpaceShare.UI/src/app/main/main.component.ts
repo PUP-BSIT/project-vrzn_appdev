360775965b35283c36508a03b606932404ec2d88
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-06-22 03:06:37 +0800

    chore: Refactor main component for pagination functionality

--- SpaceShare.UI/src/app/main/main.component.ts
+++ SpaceShare.UI/src/app/main/main.component.ts
@@ -6,12 +6,17 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css'], // Use styleUrls instead of styleUrl
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
   cards: Card[] = [];
+  paginatedCards: Card[] = [];
   subscription!: Subscription;
-  loaded = false
+  loaded = false;
+
+  currentPage: number = 1;
+  itemsPerPage: number = 1; // Change "itemsPerPage" to desired value
+  totalPages: number = 1;
 
   constructor(private mainService: MainService) {}
 
@@ -19,6 +24,8 @@ export class MainComponent implements OnInit {
     this.mainService.getProperties().subscribe(
       (data: Card[]) => {
         this.cards = data;
+        this.totalPages = Math.ceil(this.cards.length / this.itemsPerPage);
+        this.updatePaginatedCards();
       },
       (error) => {
         console.error('Error fetching properties:', error);
@@ -26,7 +33,27 @@ export class MainComponent implements OnInit {
     );
 
     setTimeout(() => {
-      this.loaded = true
-    }, 1000)
+      this.loaded = true;
+    }, 1000);
+  }
+
+  updatePaginatedCards(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedCards = this.cards.slice(startIndex, endIndex);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePaginatedCards();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePaginatedCards();
+    }
   }
 }
