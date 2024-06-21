16bbcd364293f720be3db7848591eb2517fd7f65
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-06-22 03:05:57 +0800

    add service for pagination - backend pagination

--- SpaceShare.UI/src/app/main/main.service.ts
+++ SpaceShare.UI/src/app/main/main.service.ts
@@ -1,15 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environment/appsettings';
 import { Card } from '../../model/card.model';
 
 @Injectable()
 export class MainService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getProperties(){
+  getProperties(page: number = 1, itemsPerPage: number = 10) {
     const url = `${environment.apiUrl}/property`;
-    return this.http.get<Card[]>(url);
+    const params = new HttpParams()
+      .set('page', page.toString())
+      .set('limit', itemsPerPage.toString());
+    return this.http.get<Card[]>(url, { params });
   }
 }
