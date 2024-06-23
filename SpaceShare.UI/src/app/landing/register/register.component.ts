72129024b1b5b7773e5f04252844a8329dcf6f9e
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-06-24 07:01:57 +0800

    update surname validation

--- SpaceShare.UI/src/app/landing/register/register.component.ts
+++ SpaceShare.UI/src/app/landing/register/register.component.ts
@@ -62,7 +62,7 @@ export class RegisterComponent implements OnInit {
         ],
       ],
 
-      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]*$/)]],
+      lastName: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
 
       middleName: ['', Validators.pattern(/^[a-zA-Z]*$/)],
 
