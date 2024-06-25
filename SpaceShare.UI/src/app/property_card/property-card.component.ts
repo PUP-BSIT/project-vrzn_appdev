824628c536f9e3bf57dbd4c51aab2c23dab8ef52
Jhean Galope <drakekhendrick@gmail.com>
2024-06-25 20:39:08 +0800

    update mark's delete confirmation
    Co-authored-by: Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>

--- SpaceShare.UI/src/app/property_card/property-card.component.ts
+++ SpaceShare.UI/src/app/property_card/property-card.component.ts
@@ -5,33 +5,51 @@ import { PropertyCardService } from './property-card.service';
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
-  styleUrls: ['./property-card.component.css'], 
+  styleUrls: ['./property-card.component.css'],
 })
 export class PropertyCardComponent {
   @Input() card!: Card;
   @Input() cardId!: number;
-  @Input() owned!: boolean; 
+  @Input() owned!: boolean;
   @Output() cardDeleted = new EventEmitter<number>();
-  isDeleting: boolean = false; 
+  isDeleting: boolean = false;
   isDeleted: boolean = false;
 
   constructor(private cardService: PropertyCardService) {}
 
   deleteCard(id: number) {
+    this.closeModal(id);
+    
     this.isDeleting = true;
-    this.cardService.deleteProperty(+id).subscribe((data) => {
+
+    this.cardService.deleteProperty(+this.cardId).subscribe((data) => {
       if (data.success) {
         setTimeout(() => {
           this.isDeleted = true;
           setTimeout(() => {
             this.isDeleting = false;
             this.isDeleted = false;
-            this.cardDeleted.emit(id);
+            this.cardDeleted.emit(this.cardId);
           }, 800);
         }, 2000);
       } else {
         this.isDeleting = false;
       }
     });
+
+  }
+
+  closeModal(id: number) {
+    const modal = document.getElementById(`my_modal_${id}`) as HTMLDialogElement;
+    if (modal) {
+      modal.close();
+    }
+  }
+
+  openConfirmationModal(id: number) {
+    const modal = document.querySelector(`#my_modal_${id}`) as HTMLDialogElement;
+    if (modal) {
+      modal.showModal();
+    }
   }
 }
