cb9722281ef6be67024535c522c411e306d6a053
Jhean Galope <drakekhendrick@gmail.com>
2024-06-28 16:00:04 +0800

    add routing for agreements

--- SpaceShare.UI/src/app/landing/agreement/terms/terms.component.ts
+++ SpaceShare.UI/src/app/landing/agreement/terms/terms.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.component.html',
   styleUrl: '../agreement.component.css'
 })
-export class TermsComponent {
+export class TermsComponent implements OnInit {
+  
+  ngOnInit(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 }
