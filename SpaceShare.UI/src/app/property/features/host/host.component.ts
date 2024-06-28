835ba347a27cd4ddad9881c5395ab159762d17e9
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2024-06-28 16:16:50 +0800

    Add Unmasked when logged In

--- SpaceShare.UI/src/app/property/features/host/host.component.ts
+++ SpaceShare.UI/src/app/property/features/host/host.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from '../../../../model/user.model';
 import { HostService } from './host.service';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-host',
@@ -13,21 +14,34 @@ export class HostComponent implements OnInit {
   firstName!: string;
   surname!: string;
   email!: string;
+  isLoggedIn!: boolean;
 
-  constructor(private hostService: HostService){}
+  constructor(private hostService: HostService, 
+    private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
+
     this.hostService.getUser(this.ownerId).subscribe(data => {
-      this.firstName = this.maskFirstName(data.first_name);
-      this.surname = this.maskSurname(data.surname);
-      this.phoneNumber = this.maskPhoneNumber(data.phone_number?.[0].number);
-      this.email = this.maskEmail(data.email);
+      if (this.isLoggedIn) {
+        this.firstName = data.first_name;
+        this.surname = data.surname;
+        this.phoneNumber = data.phone_number?.[0].number;
+        this.email = data.email;
+      } else {
+        this.firstName = this.maskFirstName(data.first_name);
+        this.surname = this.maskSurname(data.surname);
+        this.phoneNumber = 
+            this.maskPhoneNumber(data.phone_number?.[0].number);
+        this.email = this.maskEmail(data.email);
+      }
     });
   }
 
   maskEmail(email: string): string {
     const [name, domain] = email.split('@');
-    const maskedName = name[0] + '*'.repeat(name.length - 2) + name[name.length - 1];
+    const maskedName = name[0] + '*'.repeat(name.length - 2) +
+         name[name.length - 1];
     return `${maskedName}@${domain}`;
   }
 
@@ -40,6 +54,7 @@ export class HostComponent implements OnInit {
   }
 
   maskSurname(surname: string): string {
-    return surname[0] + '*'.repeat(surname.length - 2) + surname[surname.length - 1];
+    return surname[0] + '*'.repeat(surname.length - 2) + 
+        surname[surname.length - 1];
   }
 }
