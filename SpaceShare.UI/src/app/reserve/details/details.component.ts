31e2f237cf8ddedaa9f02b03d853d90f758d412e
Jhean Galope <drakekhendrick@gmail.com>
2024-06-30 18:56:26 +0800

    setup reserve component to be dynamic

--- SpaceShare.UI/src/app/reserve/details/details.component.ts
+++ SpaceShare.UI/src/app/reserve/details/details.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Property } from '../../../model/property.model';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
-export class DetailsComponent {
-  dates: string = 'Choose date for reservation';
+export class DetailsComponent implements OnInit {
+  @Input() property!: Property;
+  dates = 'When are you planning to move?';
   guests = {
     adults: 1,
     children: 0,
-    infants: 0
+    infants: 0,
   };
-  message: string = '';
-  modalType: string = '';
+  message!: string;
+  modalType!: string;
+  minDate!: string;
+
+  ngOnInit(): void {
+    this.minDate = this.formatDate(new Date());
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['property'] && !changes['property'].firstChange) {
+      console.log(this.property);
+    }
+  }
 
   openModal(type: string) {
     this.modalType = type;
@@ -31,11 +44,11 @@ export class DetailsComponent {
     this.closeModal();
   }
 
-  increment(type: 'adults' | 'children' | 'infants' ) {
+  increment(type: 'adults' | 'children' | 'infants') {
     this.guests[type]++;
   }
 
-  decrement(type: 'adults' | 'children' | 'infants' ) {
+  decrement(type: 'adults' | 'children' | 'infants') {
     if (this.guests[type] > 0) {
       this.guests[type]--;
     }
@@ -57,8 +70,22 @@ export class DetailsComponent {
   confirmReservation() {
     console.log('Reservation confirmed:', {
       dates: this.dates,
-      guests: this.guests
+      guests: this.guests,
+      notes: this.message
     });
     this.closeModal();
+  }
+
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Months are zero-based
+    const day = ('0' + date.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  }
+
+  validateDate(input: string): void {
+    if (new Date(input) < new Date(this.minDate)) {
+      this.dates = this.minDate;
+    }
   }
 }
--- SpaceShare.UI/src/app/reserve/reserve.component.ts
+++ SpaceShare.UI/src/app/reserve/reserve.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Property } from '../../model/property.model';
+import { ActivatedRoute } from '@angular/router';
+import { PropertyService } from '../property/property.service';
 
 @Component({
   selector: 'app-reserve',
   templateUrl: './reserve.component.html',
-  styleUrls: ['./reserve.component.css']
+  styleUrls: ['./reserve.component.css'],
 })
-export class ReserveComponent {
-  
+export class ReserveComponent implements OnInit {
+  propertyId!: number;
+  property!: Property;
+
+  constructor(
+    private route: ActivatedRoute,
+    private propertyService: PropertyService
+  ) {}
+
+  ngOnInit(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    this.route.paramMap.subscribe((params) => {
+      this.propertyId = +params.get('id')!;
+
+      setTimeout(() => {
+        this.propertyService.getProperty(this.propertyId).subscribe((data) => {
+          this.property = data;
+        });
+      }, 1000);
+    });
+  }
 }
 
 
