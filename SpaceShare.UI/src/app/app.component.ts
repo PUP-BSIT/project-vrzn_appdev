5514725bc306441905f380381053cd899efda549
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-06-30 14:04:57 +0800

    remove comments

--- SpaceShare.UI/src/app/app.component.ts
+++ SpaceShare.UI/src/app/app.component.ts
@@ -4,10 +4,10 @@ import { environment } from '../../environment/appsettings';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'], // Change "styleUrl" to "styleUrls"
+  styleUrls: ['./app.component.css'], 
 })
 export class AppComponent {
-  // Implement OnInit interface
+
   title = 'SpaceShare.UI';
   showModal: boolean = false;
   phoneNumber!: string;
