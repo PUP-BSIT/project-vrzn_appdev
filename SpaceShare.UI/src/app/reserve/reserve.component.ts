97d50caed80fcc6cde01035f9bfa8469a3176cd2
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-06-30 14:00:14 +0800

    make reservation component content

---
+++ SpaceShare.UI/src/app/reserve/reserve.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserve',
+  templateUrl: './reserve.component.html',
+  styleUrls: ['./reserve.component.css']
+})
+export class ReserveComponent {
+  
+}
+
+
+
+
+
