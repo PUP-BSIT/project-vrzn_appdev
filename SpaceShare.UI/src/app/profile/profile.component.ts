3ec5fd1274c3135a7dd04973d0a134d7d0dbbf6e
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-07-02 10:39:52 +0800

    improve match pass validator

--- SpaceShare.UI/src/app/profile/profile.component.ts
+++ SpaceShare.UI/src/app/profile/profile.component.ts
@@ -1,12 +1,11 @@
-import {
-  Component, Input, OnChanges,
-  OnInit, SimpleChanges } from '@angular/core';
-import {
-  AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User, UserToUpdate } from '../../model/user.model';
 import { CustomValidators } from './custom-validators';
 import { CookieService } from 'ngx-cookie-service';
 import { ProfileService } from './profile.service';
+import { PasswordValidator } from '../profile/custom-validators';
+import { PasswordMatchValidator } from './custom-validators';
 
 @Component({
   selector: 'app-profile',
@@ -19,6 +18,7 @@ export class ProfileComponent implements OnInit {
   isForgot = false;
   currentUser!: User;
   user_id = this.cookie.get('id');
+  passForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -31,26 +31,35 @@ export class ProfileComponent implements OnInit {
       this.currentUser = user;
     });
 
+    this.passForm = this.formBuilder.group({
+      old_password: ['', [Validators.required, Validators.minLength(8)]],
+      password: ['', [PasswordValidator.strong]],
+      confirmPassword: ['', Validators.required],
+    }, {
+      validators: PasswordMatchValidator.passwordsMatch()
+    });
+
     this.editForm = this.formBuilder.group({
-      firstName: ['',
+      firstName: [
+        '',
         [
           Validators.required,
           Validators.maxLength(60),
           Validators.pattern('^[a-zA-ZñÑ \\-]*$'),
-        ]],
-
-      lastName: ['', 
+        ],
+      ],
+      lastName: [
+        '',
         [
-          Validators.required,  
+          Validators.required,
           Validators.pattern('^[a-zA-ZñÑ \\-]*$'),
-          Validators.maxLength(60)
-        ]],
-
+          Validators.maxLength(60),
+        ],
+      ],
       phoneNumber: [
         '',
         [Validators.required, Validators.pattern(/^[0-9]{11}$/)],
       ],
-
       birthdate: [
         '',
         [Validators.required, CustomValidators.adultAgeValidator('birthdate')],
@@ -62,12 +71,11 @@ export class ProfileComponent implements OnInit {
     this.isEditMode = true;
     this.isForgot = false;
   }
-  
+
   Forgot() {
     this.isForgot = true;
     this.isEditMode = false;
   }
-
 
   initializeForm(user: User): void {
     this.editForm.patchValue({
@@ -76,6 +84,14 @@ export class ProfileComponent implements OnInit {
       phoneNumber: user.phone_number[0].number,
       birthdate: this.formatDate(user.birthdate.toString()),
     });
+  }
+
+  get passwordControl(): AbstractControl {
+    return this.passForm.get('password')!;
+  }
+
+  get confirmPasswordControl(): AbstractControl {
+    return this.passForm.get('confirmPassword')!;
   }
 
   get firstNameControl(): AbstractControl {
@@ -94,45 +110,53 @@ export class ProfileComponent implements OnInit {
     return this.editForm.get('birthdate')!;
   }
 
+  clearError(controlName: string) {
+    this.passForm.get(controlName)?.markAsUntouched();
+  }
+
+  onReset(): void {
+    this.passForm.reset();
+  }
+
   onSubmit(): void {
     this.isEditMode = false;
     if (!this.editForm.dirty && this.editForm.touched) return;
 
-
     const { firstName, lastName, phoneNumber, birthdate } = this.editForm.value;
 
-    if (this.currentUser.first_name === firstName &&
+    if (
+      this.currentUser.first_name === firstName &&
       this.currentUser.surname === lastName &&
       this.currentUser.phone_number[0].number === phoneNumber &&
-      this.formatDate(this.currentUser.birthdate.toString()) === birthdate) {
+      this.formatDate(this.currentUser.birthdate.toString()) === birthdate
+    ) {
       return;
     }
 
     const user: UserToUpdate = {
-        id: +this.user_id,
-        first_name: firstName,
-        surname: lastName,
-        birthdate: birthdate
-    }
+      id: +this.user_id,
+      first_name: firstName,
+      surname: lastName,
+      birthdate: birthdate,
+    };
 
     const dataToUpdate = {
       user: user,
       oldPhoneNumber: this.currentUser.phone_number[0].number,
-      newPhoneNumber: phoneNumber
+      newPhoneNumber: phoneNumber,
     };
 
-
-    this.profileService.updateUserProfile(dataToUpdate).subscribe(data => {
+    this.profileService.updateUserProfile(dataToUpdate).subscribe((data) => {
       location.reload();
-    })
+    });
 
     this.editForm.reset();
   }
 
-
   cancelChanges(): void {
     this.isEditMode = false;
     this.isForgot = false;
+    this.passForm.reset();
   }
 
   formatDate(dateString: string, use?: string): string {
