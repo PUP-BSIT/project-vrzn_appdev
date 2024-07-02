699894b8940a8c53d076648467ffc2a303603221
Jhean Galope <drakekhendrick@gmail.com>
2024-07-03 00:16:06 +0800

    implement resetpass service

--- SpaceShare.UI/src/app/landing/reset_password/reset-password.service.ts
+++ SpaceShare.UI/src/app/landing/reset_password/reset-password.service.ts
@@ -1,18 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environment/appsettings';
+import { CommonResponse } from '../../../model/user.model';
 
 @Injectable()
 export class ResetPasswordService {
 
+  url = `${environment.apiUrl}/auth`
+
   constructor(private http: HttpClient) { }
 
   forgotPassword(email: string){
-    const url = `${environment.apiUrl}/auth/forgot`
+    const apiUrl = `${this.url}/forgot`
+    return this.http.post(apiUrl, { email: email });
+  }
 
-    console.log(email);
-
-    return this.http.post(url, { "email": email });
+  resetPassword(token: string, newPassword: string){
+    const apiUrl = `${this.url}/reset`;
+    return this.http.post<CommonResponse>(apiUrl, { 
+      token: token,
+      newPassword: newPassword
+    })
   }
 
 }
