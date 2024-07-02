b7701dba58608c5954c2801cfa8f74c4d26188b3
Naoe-Adrian <adrianbnaoe@iskolarngbayan.pup.edu.ph>
2024-07-02 20:21:13 +0800

    update reactive border error

--- SpaceShare.UI/src/app/landing/reset_password/reset-form/reset-form.component.ts
+++ SpaceShare.UI/src/app/landing/reset_password/reset-form/reset-form.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder, AbstractControl } from '@angular/forms';
-import { PasswordValidator,PasswordMatchValidator } from './custom-validator';
+import { PasswordValidator, PasswordMatchValidator } from './custom-validator';
+
 @Component({
   selector: 'app-reset-form',
   templateUrl: './reset-form.component.html',
-  styleUrl: './reset-form.component.css'
+  styleUrls: ['./reset-form.component.css']
 })
-
-export class ResetFormComponent {
+export class ResetFormComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {}
   passForm!: FormGroup;
 
@@ -23,10 +23,10 @@ export class ResetFormComponent {
       }
     );
   }
-  
 
   onSubmit() {
-
+    if (!this.passForm.valid) return;
+    // Handle form submission logic
   }
 
   get passwordControl(): AbstractControl {
@@ -41,4 +41,17 @@ export class ResetFormComponent {
     this.passForm.get(controlName)?.markAsUntouched();
   }
 
+  getValidationClass(control: AbstractControl): string {
+    if (this.passForm.hasError('passwordsMismatch') && control.touched) {
+      return 'border-red-500';
+    } else if (control.valid && control.touched) {
+      return 'bg-green-50 border border-green-500 text-green-900 placeholder-green-700';
+    } else if (control.invalid && control.touched) {
+      return 'border-red-500';
+    } else if (control.value) {
+      return 'bg-green-50 border border-green-500 text-green-900 placeholder-green-700';
+    } else {
+      return '';
+    }
+  }
 }
