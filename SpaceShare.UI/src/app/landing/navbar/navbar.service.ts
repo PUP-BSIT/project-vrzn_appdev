44db560a279464fc7a8fe279bc6f5ab21103b5e1
Jhean Galope <drakekhendrick@gmail.com>
2024-07-04 20:24:53 +0800

    setup notification service

--- SpaceShare.UI/src/app/landing/navbar/navbar.service.ts
+++ SpaceShare.UI/src/app/landing/navbar/navbar.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../environment/appsettings';
+import { AuthService } from '../../auth/auth.service';
 
 // Define a type for the response data
 interface LoginResponse {
@@ -13,16 +14,30 @@ interface LoginResponse {
 
 @Injectable()
 export class NavbarService {
-  constructor(
-  private http: HttpClient,
-  ) {}
+  constructor(private http: HttpClient) {}
 
   signout() {
     const url = `${environment.apiUrl}/auth/signout`;
     return this.http.get<LoginResponse>(url);
-  } 
+  }
 
-  getNotification(){
-    
+  getReservationNotification(userId: number) {
+    const url = `${environment.apiUrl}/event/notification/reservation/${userId}`;
+    return this.http.get(url);
+  }
+
+  getApplicationNotification(userId: number) {
+    const url = `${environment.apiUrl}/event/notification/application/${userId}`;
+    return this.http.get(url);
+  }
+
+  setApplicationNotificationAsRead(userId: number) {
+    const url = `${environment.apiUrl}/event/notified/application`;
+    return this.http.post(url, { userId });
+  }
+
+  setReservationNotificationAsRead(userId: number) {
+    const url = `${environment.apiUrl}/event/notified/reservation`;
+    return this.http.post(url, { userId });
   }
 }
