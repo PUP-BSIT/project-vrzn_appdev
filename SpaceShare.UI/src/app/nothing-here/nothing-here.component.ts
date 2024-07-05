7e5814b5c5f0503e0b4d91d9298aedb22e0ad71a
Odrunia_Mark_Ryan <odruniamarkryan@gmail.com>
2024-07-05 19:54:41 +0800

    add new component for "nothing to see here" card

---
+++ SpaceShare.UI/src/app/nothing-here/nothing-here.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nothing-here',
+  templateUrl: './nothing-here.component.html',
+  styleUrl: './nothing-here.component.css'
+})
+export class NothingHereComponent {
+
+}
