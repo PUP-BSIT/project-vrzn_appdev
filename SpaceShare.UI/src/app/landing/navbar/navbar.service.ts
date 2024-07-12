00e3136544c866191b753ece716b5a68afde3ce1
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-07-12 11:58:01 +0800

    Code review: Align navbar.component with coding guidelines for improved readability

--- SpaceShare.UI/src/app/landing/navbar/navbar.service.ts
+++ SpaceShare.UI/src/app/landing/navbar/navbar.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../environment/appsettings';
 
-// Define a type for the response data
 interface LoginResponse {
   success: boolean;
   message: string;
-  id: string,
-  token: string
+  id: string;
+  token: string;
 }
-
 
 @Injectable()
 export class NavbarService {
